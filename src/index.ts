dad2925437b96d639ac430ff139b7b0827e01f46
gavintranquilino <gavintranquilino@gmail.com>
2025-03-18 00:00:16 -0400

    Send message in our discord server when POST request is received on localhost

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import { Client, Collection, GatewayIntentBits, Events, MessageFlags } from "discord.js";
+import { Client, Collection, GatewayIntentBits, Events, MessageFlags, TextChannel } from "discord.js";
 import "dotenv/config"; // to use .env to store sensitive data
 import fs from "node:fs"; // to use file system (node builtin)
 import path from "node:path"; // to use paths (node builtin)
+import { app } from "./server"; // import the webserver defined in server.ts
 
 const TOKEN = process.env.DISCORD_BOT_TOKEN;
 const CLIENT_ID = process.env.DISCORD_CLIENT_ID;
@@ -9,7 +10,7 @@ if (!TOKEN || !CLIENT_ID) throw new Error("Missing environment variables");
 
 // Create bot client
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds],
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
 });
 
 // Loading command files
@@ -24,7 +25,7 @@ const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
     const commandsPath = path.join(foldersPath, folder);
-    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.ts'));
     for (const file of commandFiles) {
         const filePath = path.join(commandsPath, file);
         import(filePath).then(command => {
@@ -41,11 +42,17 @@ for (const folder of commandFolders) {
 // Bot ready event
 client.once("ready", () => {
   console.log(`Logged in as ${client.user?.tag}`);
+  
+  // Log all available guilds
+  console.log("Available guilds:");
+  client.guilds.cache.forEach(guild => {
+    console.log(`- ${guild.name} (ID: ${guild.id})`);
+  });
 });
 
 // Receive interaction /handle command interactions
 client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
+    if (!interaction.isChatInputCommand()) return;
 
   const command = interaction.client.commands.get(interaction.commandName);
 
@@ -63,6 +70,31 @@ client.on(Events.InteractionCreate, async interaction => {
     } else {
       await interaction.reply({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
     }
+  }
+});
+
+// API endpoint to receive messages from the Chrome extension
+app.post('/trigger', (req, res) => {
+  console.log('Received message', req.body);
+  const { message } = req.body;
+
+  // Get the guild and the channel within the guild
+  const guild = client.guilds.cache.get('1112885669912658041');
+  if (!guild) {
+    res.status(500).send('Guild not found');
+    console.log('Guild not found');
+    return;
+  }
+
+  const channel = guild.channels.cache.get('1112885670361452685') as TextChannel;
+  if (channel && channel.isTextBased()) {
+    channel.send(message);
+    res.status(200).send('Message sent');
+    console.log('Message sent');
+  } 
+  else {
+    res.status(500).send('Channel not found');
+    console.log('Channel not found');
   }
 });
 
